7658cbc0a1beeddc539c6acbd03853b5cb169238
caoimhejyoti <caoimhejyoti@gmail.com>
2022-11-27 14:17:12 +0800

    initial notes and coding ideas

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,80 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+// worked way through js doc from top to bottom, following console error messages. 
+// first message - error in line 2. changed generate to password due ot generate not being present in CSS. Realised error - it is present in HTML and changedf back. 
+// next step was to connect line 14 (js event) to html line 29.
+// next error points to writePassword function. Need to resolve function bugs to ensure generate button triggers event.
+
+// psuedo code:
+// user needs to define number of charaters - prompt
+
+// var options = (any Number)?
+
+// var userChoice = put user input here later
+
+
+// user defines special charcter prompt - true or false
+// user defines numbers prompt - true or false
+// user defines lower case prompt - true or false
+// user defines upper case prompt - true or false
+
+// computer produces random string - using user options as input
+
+// display the password in card body. 
+
+// click on generateBtn - produces prompt
+
+
+
+
+// var passwordLength = window.prompt("How many characters does your password need? It must be between 8 - 128 characters")
+// var specicalCharacter = confirm("Do you want to include special characters?")
+
+// var lowerCharacter = confirm("Do you want to include lower case charaters?")
+// var upperCharacter = confirm("Do you want to include upper case charaters?")
+
+// var options = all user choice defined charaters
+
+var generateBtn = document.querySelector("#generate");
+
+
+
+
+var numberOptions = ["1", "2", "3","4", "5", "6", "7", "8", "9", "0"];
+var lowerOptions = ["a", "b"]
+
+
 function writePassword() {
-  var password = generatePassword();
+  var numberCharacter = confirm("Do you want to include numbers?")
+  if(!numberCharacter) {
+
+  }
+  var lowerCharacter = confirm("Do you want to include lower case charaters?")
+  if(!lowerCharacter) {
+    return;
+  }
+
+  var generatePassword = Math.floor(Math.random() *numberOptions.length)+ (Math.random() *lowerOptions.length)
+
+  var password = generatePassword;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
